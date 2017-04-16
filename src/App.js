54d5008becef22474bedd63447c6c7217269419f
doodybrains <doodybrainz@gmail.com>
2017-04-15 23:46:45 -0400

    first pass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 require('./app.scss');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>New App With Sass</h2>
+      <div className="container">
+      <div className="home-link">
+        <a className="title-card">
+          <p>d</p>
+          <p>e</p>
+          <p>e</p>
+          <p>p</p>
+          <br/>
+          <p>n</p>
+          <p>e</p>
+          <p>w</p>
+          <p>s</p>
+          <br/>
+          <p>c</p>
+          <p>l</p>
+          <p>u</p>
+          <p>b</p>
+        </a>
+      </div>
+
+
+        <div className="news-mod">
+          <div className="title">
+            <h3>debunking</h3>
+            <a>PIZZAGATE</a>
+          </div>
+
+          <div className="mod-list">
+            <div className="module-1">
+              <p>First there was this thing that happened</p>
+            </div>
+            <div className="module-2">
+              <p>It looked kind of like this</p>
+              <img src="https://i.ytimg.com/vi/aEtm69mLK6w/mqdefault.jpg" />
+            </div>
+            <div className="module-3">
+              <p>Then this happened</p>
+            </div>
+
+            <div className="module-3">
+              <p>and so on</p>
+            </div>
+
+            <div className="module-3">
+              <p>and so on and so on</p>
+            </div>
+
+            <div className="module-3">
+              <p>and so on and so on and son on ands on</p>
+            </div>
+          </div>
         </div>
       </div>
     );
